5ef05d5fa7c4502cbe523372a42f331bbb1e9dab
Matas Kulingauskas <matask@gmail.com>
2018-11-20 22:54:40 +0200

    Padarytas ImageSlicer service'as (nepabaigtas)

---
+++ src/Service/ImageSlicer.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matas
+ * Date: 2018-11-19
+ * Time: 20:54
+ */
+
+namespace App\Service;
+
+
+class ImageSlicer
+{
+    private $imagePath;
+    private $slicesPath;
+    private $image;
+    
+    public function __construct(string $imagePath, string $slicesPath)
+    {
+        if(!file_exists($imagePath)){
+            echo 'file not exists';
+            exit;
+        }
+        
+        $this->image = imagecreatefromjpeg($imagePath);
+        if($this->image === false){
+            echo 'imagecreatefromjpeg error';
+            exit;
+        }
+    
+        $this->slicesPath = $slicesPath;
+    }
+    
+    public function run(array $slices): array
+    {
+        $image_preix = time();
+        
+        foreach($slices as &$slice){
+            $resurce = $this->crop($slice['box']);
+            $status = imagepng($resurce, $this->slicesPath.$image_preix.'_'.$slice['key'].'.png');
+            imagedestroy($resurce);
+            if($status === false){
+                echo 'imagepng error: '.$this->slicesPath.$image_preix.'_'.$slice['key'].'.png';
+                exit;
+            }
+            
+            $slice['file_name'] = $image_preix.'_'.$slice['key'].'.png';
+            $slice['file_path'] = $this->slicesPath.$image_preix.'_'.$slice['key'].'.png';
+        }
+        unset($slice);
+        
+        return $slices;
+    }
+    
+    public function crop($box)
+    {
+        $im2 = imagecrop($this->image, $box);
+        if ($im2 === false) {
+            echo 'Crop error';
+            exit;
+        }
+        return $im2;
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getImagePath()
+    {
+        return $this->imagePath;
+    }
+    
+    /**
+     * @param mixed $imagePath
+     */
+    public function setImagePath($imagePath): void
+    {
+        $this->imagePath = $imagePath;
+    }
+}
