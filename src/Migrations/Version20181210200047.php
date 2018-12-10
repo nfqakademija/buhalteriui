231aefac59b32ea0a19e30e2ed44dc7d28ffdde5
Matas Kulingauskas <matask@gmail.com>
2018-12-10 22:01:25 +0200

    Migration fix

--- src/Migrations/Version20181210200047.php
+++ src/Migrations/Version20181210200047.php
@@ -8,14 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181210183124 extends AbstractMigration
+final class Version20181210200047 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE template (template_id INT UNSIGNED AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, parameters JSON DEFAULT NULL, create_time DATETIME NOT NULL, PRIMARY KEY(template_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE template (template_id INT UNSIGNED AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, parameters LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', create_time DATETIME NOT NULL, PRIMARY KEY(template_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
