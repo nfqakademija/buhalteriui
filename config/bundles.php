3f020056578d9b5991f14d4df4ede5eddd26667a
Matas Kulingauskas <matask@gmail.com>
2018-12-10 20:41:33 +0200

    Pridėtas Fixtures Bundle

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
