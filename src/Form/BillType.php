d911ef6ef9135b9b31c9867beddd6d805b830af8
Matas Kulingauskas <matask@gmail.com>
2018-12-13 23:09:17 +0200

    Dokumento įkelimo formoje įdėtas bylos tikrinimas

--- src/Form/BillType.php
+++ src/Form/BillType.php
@@ -4,9 +4,10 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class BillType extends AbstractType
 {
@@ -14,7 +15,43 @@ class BillType extends AbstractType
     {
         $builder
             ->add('bill', FileType::class, [
-                'attr' => ['submit' => 'form-control']
+//                'required' => true,
+                'attr' => ['submit' => 'form-control'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Nurodykite dokumentą.',
+                    ]),
+                    new Image([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/jpg',
+                            //'image/png',
+                        ],
+                        'allowSquare' => false,
+                        'detectCorrupted' => true,
+                        'minWidth' => 1654,
+                        'maxWidth' => 1654,
+                        'minHeight' => 2339,
+                        'maxHeight' => 2339,
+                        'maxSizeMessage' => 'Įkeliamas dokumentas yra per didelis ({{ size }} {{ suffix }}). ' .
+                            'Maksimalus dydis yra {{ limit }} {{ suffix }}.',
+                        'mimeTypesMessage' => 'Prašome nurodyti JPG bylą',
+                        'disallowEmptyMessage' => 'Prašome nurodyti dokumentą.',
+                        'maxWidthMessage' => 'Dokumentas per platus ({{ width }}px). ' .
+                            'Leidžiamas plotis {{ max_width }}px.',
+                        'minWidthMessage' => 'Dokumentas per siauras ({{ width }}px). ' .
+                            'Leidžiamas plotis {{ min_width }}px.',
+                        'maxHeightMessage' => 'Dokumentas per aukštas ({{ height }}px). ' .
+                            'Leidžiamas aukštis {{ max_height }}px.',
+                        'minHeightMessage' => 'Dokumentas per mažas ({{ height }}px). ' .
+                            'Leidžiamas aukštis {{ min_height }}px.',
+                        'sizeNotDetectedMessage' => 'Nepavyko nustatyti dokumento matmenų',
+                        'allowSquareMessage' => 'Dokumento kraštinės yra lygios ({{ width }}x{{ height }}px). ' .
+                            'Paveiksliukas turi būti stačiakampis.',
+                        'corruptedMessage' => 'Nepavyko nuskaityti dokumento, greičiausiai byla sugadinta.',
+                    ]),
+                ]
             ])
             ->add('submit', SubmitType::class, [
                 'attr' => ['class' => 'btn btn-success btn-lg btn-block']
