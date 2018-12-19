f968b93704fef44337568d080ff7c20a07686ac8
Matas Kulingauskas <matask@gmail.com>
2018-12-19 23:16:46 +0200

    Pašalintos kodo šiukšlės

--- src/Repository/DocumentRepository.php
+++ src/Repository/DocumentRepository.php
@@ -23,33 +23,4 @@ class DocumentRepository extends ServiceEntityRepository
     {
         return $this->createQueryBuilder('Document');
     }
-    
-    // /**
-    //  * @return Document[] Returns an array of Document objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('d')
-            ->andWhere('d.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('d.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Document
-    {
-        return $this->createQueryBuilder('d')
-            ->andWhere('d.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
